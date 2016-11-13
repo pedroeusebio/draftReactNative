34014bc97cd3a2abbbbd3a21bff40dd8943a5301
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-11-13 14:57:12 -0200

    Adding icons to the Top bar;

--- src/header.js
+++ src/header.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome.js';
 
 
 export class Header extends Component {
   render() {
     return (
       <View style={styles.header}>
+        <Icon style={styles.iconLeft} name='bars' size={25} color="#ededed" onPress={() => {
+            this.props.trigger();
+          }}/>
         <Text style={styles.title}>{ this.props.title }</Text>
+        <Icon style={styles.iconRight} name='plus' size={25} color="#ededed"/>
+
       </View>
     );
   }
@@ -15,17 +21,21 @@ export class Header extends Component {
 
 const styles = StyleSheet.create({
   header: {
-    flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'space-around',
-    height: 40,
-    paddingTop: 10,
-    marginBottom: 5,
-    backgroundColor: 'skyblue',
+    paddingTop: 20,
+    backgroundColor: '#FF9466',
+  },
+  iconLeft: {
+    paddingLeft: 10
+  },
+  iconRight: {
+    paddingRight: 10
   },
   title: {
     color: '#fff',
-    fontSize: 16
+    fontSize: 16,
+    alignSelf: 'center'
   }
 });
