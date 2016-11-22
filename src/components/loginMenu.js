7d85ca039ef4bcd28a7c85ec96e63ccb91ea4aa1
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-11-22 11:19:24 -0200

    finishing login screen;

--- src/components/loginMenu.js
+++ src/components/loginMenu.js
@@ -13,6 +13,7 @@ export class LoginMenu extends Component {
   button(title, icon, style, increment) {
     return(
    <Button
+      raised
      icon={{name: icon, type: 'font-awesome', color: '#9a5c75'}}
      title={title}
      buttonStyle={style}
@@ -24,16 +25,15 @@ export class LoginMenu extends Component {
 
   buttonLang(flag) {
     return (
+      <TouchableHighlight  onPress={() => console.log(this)}>
       <View style={styles.langButton}>
-        <TouchableHighlight onPress={() => console.log('pertei')}>
           <Image
             style={styles.lang}
             source={flag}
           />
-        </TouchableHighlight>
-      </View>
+         </View>
+      </TouchableHighlight>
       );
-
   }
 
   render() {
@@ -48,21 +48,28 @@ export class LoginMenu extends Component {
             source={require('../../public/img/moveLogo.png')}
           />
         </View>
+      <View style={styles.langContent}>
+      <View style={styles.textLang}>
+      <Text style={{color: '#f3f4f6'}}>Selecione o idioma desejado :</Text>
+      </View>
         <View style={styles.langView}>
           { this.buttonLang(flags.BR) }
           { this.buttonLang(flags.US) }
           { this.buttonLang(flags.ES) }
         </View>
+      </View>
+      <View style={styles.buttonContent}>
         <View style={styles.buttonView}>
-          {this.button('Entrar com E-mail', 'envelope-o' , styles.button, increment)}
+          {this.button('Entrar com e-mail', 'envelope-o' , styles.button, increment)}
         </View>
         <View style={styles.buttonView}>
           { this.button('Entrar com Facebook', 'facebook' , styles.button, increment) }
         </View>
-        {/* <View style={styles.registerView}>
-            <Text>Ainda não tem cadastro ?</Text>
-            <Text>Cadastre-se já</Text>
-            </View> */}
+      </View>
+      <View style={styles.registerView}>
+        <Text style={{color: '#f3f4f6'}}>Ainda não tem cadastro ?</Text>
+        <Text style={{color:'#f0b7a6', fontSize: 20, lineHeight: 20 }}>Cadastre-se já</Text>
+      </View>
       </LinearGradient>
     </View>
     );
@@ -72,43 +79,58 @@ export class LoginMenu extends Component {
 const styles = StyleSheet.create({
   linearGradient: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: 15,
-    flexDirection: 'column'
+    justifyContent: 'space-between',
   },
   loginView: {
     backgroundColor: '#f5fcff',
     flex: 1,
+    flexDirection: 'column'
   },
-  button: {
-    borderRadius: 15,
-    backgroundColor: '#FFF',
-  },
-  buttonView: {
-    paddingBottom: 20
+
+  langContent: {
+    justifyContent: 'space-between'
   },
   langView: {
     justifyContent: 'space-around',
     flexDirection: 'row',
-    paddingBottom: 40
-  },
-  lang: {
+    paddingBottom: 30
   },
   langButton: {
     justifyContent: 'center',
     alignItems: 'center',
     width: 90,
     height: 40,
-    backgroundColor: 'white',
+    borderWidth: 2,
+    borderColor: '#f3f4f6',
     borderRadius: 10
   },
-  logoView: {
+  textLang: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    alignSelf: 'center',
+    marginBottom: 10
+  },
+  buttonContent: {
+    marginBottom: 20
+  },
+  button: {
+    borderRadius: 10,
+    backgroundColor: '#f3f4f6',
+  },
+  buttonView: {
+    paddingBottom: 10
+  },
+    logoView: {
     justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: 40
+    marginTop: 80
   },
   logo: {
     width: 250,
     height: 110
+  },
+  registerView: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    alignItems: 'center',
+    marginBottom: 90
   }
 });
